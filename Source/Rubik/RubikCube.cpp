eec381ef5eb28d0776bb694a5eb93b328eb0649c
Leandro Geminale <thedeolink93@gmail.com>
2019-11-28 11:35:36 +0100

    Initial settings + added Rubik class
    Rubik class with the basic components:
    2 scene components: one to rotate the cubes, one to be the root of the pawn
    Spring arm + Camera: to use to rotate the viewport around

---
+++ Source/Rubik/RubikCube.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RubikCube.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Classes/Camera/CameraComponent.h"
+
+// Sets default values
+ARubikCube::ARubikCube()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Initializing the components
+	CameraSpringArm = CreateDefaultSubobject<USpringArmComponent>("CameraSpringArm");
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Root = CreateDefaultSubobject<USceneComponent>("Root");
+	RotatingRoot = CreateDefaultSubobject<USceneComponent>("RotatingRoot");
+
+	// Seting up the components tree
+	RootComponent = Root;
+	CameraSpringArm->SetupAttachment(RootComponent);
+	RotatingRoot->SetupAttachment(RootComponent);
+	Camera->SetupAttachment(CameraSpringArm);
+
+}
+
+// Called when the game starts or when spawned
+void ARubikCube::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	
+}
+
+// Called every frame
+void ARubikCube::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ARubikCube::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
